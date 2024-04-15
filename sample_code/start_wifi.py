fe3b098d983742e33ea9c61f342321ddb8eeb8a4
Kent Hutson <kenthutson@gmail.com>
2024-04-15 15:20:29 -0600

    Create start_wifi.py

---
+++ sample_code/start_wifi.py
@@ -0,0 +1,31 @@
+# wlan = network.WLAN(network.STA_IF) # create station interface
+# wlan.active(True)       # activate the interface
+# wlan.scan()             # scan for access points
+# wlan.isconnected()      # check if the station is connected to an AP
+# wlan.connect('ssid', 'key') # connect to an AP
+# wlan.config('mac')      # get the interface's MAC address
+# wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
+# 
+# ap = network.WLAN(network.AP_IF) # create access-point interface
+# ap.config(ssid='ESP-AP') # set the SSID of the access point
+# ap.config(max_clients=10) # set how many clients can connect to the network
+# ap.active(True)         # activate the interface
+# wlan.config(reconnects=n)
+
+
+
+def do_connect(hostname=None):
+    import network
+    if hostname is not None:
+        network.hostname(hostname)
+    wlan = network.WLAN(network.STA_IF)
+#     wlan.config(reconnects=5)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('art-iot', 'artisfun')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+# do_connect('hutson-iot')
