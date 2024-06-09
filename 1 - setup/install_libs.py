0acc8abe14699ca4cba75318bb9bf1ac801b0670
Kent Hutson <kenthutson@gmail.com>
2024-06-09 13:53:03 -0600

    Create install_libs.py

---
+++ 1 - setup/install_libs.py
@@ -0,0 +1,4 @@
+import mip
+mip.install('https://github.com/MonumentalImpact/IOT_workshop/blob/main/libs/servo.py')
+mip.install('https://github.com/MonumentalImpact/IOT_workshop/blob/main/libs/ssd1306.py')
+mip.install('https://github.com/MonumentalImpact/IOT_workshop/blob/main/libs/stepper.py')
