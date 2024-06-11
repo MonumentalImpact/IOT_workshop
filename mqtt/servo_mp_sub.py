4c6cb45bb27c15ba68258fbc1aad6a4da14f6343
Kent Hutson <kenthutson@gmail.com>
2024-06-11 13:55:26 -0600

    Create servo_mp_sub.py

---
+++ mqtt/servo_mp_sub.py
@@ -0,0 +1,51 @@
+from umqtt.simple import MQTTClient
+from time import sleep
+from servo import Servo
+
+servos = []
+
+#for MS18 servo, orange=signal(attach to control pin, this example uses 23), red=+5v, green=GND
+max_angle = 180
+s = Servo( pin=23, freq=50, min_us=500, max_us=2500, angle=max_angle) 
+
+#
+# Publish test messages e.g. with:
+# mosquitto_pub -t foo_topic -m hello
+# or see esp32 example at
+# https://github.com/MonumentalImpact/IOT_workshop/blob/main/mqtt/test_mp_pub.py
+
+
+CLIENT_NAME = 'mp1' #must be set to the name of the esp32 being used
+BROKER_ADDR = 'pi5ub.local'
+
+emo = 'unknown'
+
+# Received messages from subscriptions will be delivered to this callback
+def sub_cb(topic, msg):
+    global emo
+    print((topic, msg))
+    if topic == b'art/emotion':
+        emo = msg.decode('utf-8')
+            
+
+def main(server="localhost"):
+    c = MQTTClient(CLIENT_NAME, BROKER_ADDR)
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(b'art/emotion')
+    i = 0
+    while True:
+        # Non-blocking wait for message
+        c.check_msg()
+        # Then need to sleep to avoid 100% CPU usage (in a real
+        # app other useful actions would be performed instead)
+        sleep(1)
+        i+=1
+        if i > 30:
+            break
+
+    c.disconnect()
+
+
+if __name__ == "__main__":
+    main()
